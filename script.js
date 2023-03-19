6250f437d8df0571a3aa0050f45408ae3bdf39de
nexon <nexon70000@gmail.com>
2023-03-19 19:14:39 +0100

    a

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ function changeContent(content){
             document.getElementsByClassName('accueil')[0].classList.remove('hidden');
             document.getElementsByClassName('accueil')[0].classList.add('visible');
         }
+        else if(document.getElementsByClassName('projet')[0].classList.contains('visible')){
+            document.getElementsByClassName('projet')[0].classList.remove('visible');
+            document.getElementsByClassName('projet')[0].classList.add('hidden');
+            document.getElementsByClassName('accueil')[0].classList.remove('hidden');
+            document.getElementsByClassName('accueil')[0].classList.add('visible');
+        }
+        else if(document.getElementsByClassName('propos')[0].classList.contains('visible')){
+            document.getElementsByClassName('propos')[0].classList.remove('visible');
+            document.getElementsByClassName('propos')[0].classList.add('hidden');
+            document.getElementsByClassName('accueil')[0].classList.remove('hidden');
+            document.getElementsByClassName('accueil')[0].classList.add('visible');
+        }
     }
     else if(content==="competence"){
         if(document.getElementsByClassName('accueil')[0].classList.contains('visible')){
@@ -13,6 +25,59 @@ function changeContent(content){
             document.getElementsByClassName('accueil')[0].classList.add('hidden');
             document.getElementsByClassName('competence')[0].classList.remove('hidden');
             document.getElementsByClassName('competence')[0].classList.add('visible');
+        }
+        else if(document.getElementsByClassName('projet')[0].classList.contains('visible')){
+            document.getElementsByClassName('projet')[0].classList.remove('visible');
+            document.getElementsByClassName('projet')[0].classList.add('hidden');
+            document.getElementsByClassName('competence')[0].classList.remove('hidden');
+            document.getElementsByClassName('competence')[0].classList.add('visible');
+        }
+        else if(document.getElementsByClassName('propos')[0].classList.contains('visible')){
+            document.getElementsByClassName('propos')[0].classList.remove('visible');
+            document.getElementsByClassName('propos')[0].classList.add('hidden');
+            document.getElementsByClassName('competence')[0].classList.remove('hidden');
+            document.getElementsByClassName('competence')[0].classList.add('visible');
+        }
+    }
+    else if(content==="projet"){
+        if(document.getElementsByClassName('accueil')[0].classList.contains('visible')){
+            document.getElementsByClassName('accueil')[0].classList.remove('visible');
+            document.getElementsByClassName('accueil')[0].classList.add('hidden');
+            document.getElementsByClassName('projet')[0].classList.remove('hidden');
+            document.getElementsByClassName('projet')[0].classList.add('visible');
+        }
+        else if(document.getElementsByClassName('propos')[0].classList.contains('visible')){
+            document.getElementsByClassName('propos')[0].classList.remove('visible');
+            document.getElementsByClassName('propos')[0].classList.add('hidden');
+            document.getElementsByClassName('projet')[0].classList.remove('hidden');
+            document.getElementsByClassName('projet')[0].classList.add('visible');
+        }
+        else if(document.getElementsByClassName('competence')[0].classList.contains('visible')){
+            document.getElementsByClassName('competence')[0].classList.remove('visible');
+            document.getElementsByClassName('competence')[0].classList.add('hidden');
+            document.getElementsByClassName('projet')[0].classList.remove('hidden');
+            document.getElementsByClassName('projet')[0].classList.add('visible');
+        }
+
+    }
+    else if(content==="propos"){
+        if(document.getElementsByClassName('accueil')[0].classList.contains('visible')){
+            document.getElementsByClassName('accueil')[0].classList.remove('visible');
+            document.getElementsByClassName('accueil')[0].classList.add('hidden');
+            document.getElementsByClassName('propos')[0].classList.remove('hidden');
+            document.getElementsByClassName('propos')[0].classList.add('visible');
+        }
+        else if(document.getElementsByClassName('competence')[0].classList.contains('visible')){
+            document.getElementsByClassName('competence')[0].classList.remove('visible');
+            document.getElementsByClassName('competence')[0].classList.add('hidden');
+            document.getElementsByClassName('propos')[0].classList.remove('hidden');
+            document.getElementsByClassName('propos')[0].classList.add('visible');
+        }
+        else if(document.getElementsByClassName('projet')[0].classList.contains('visible')){
+            document.getElementsByClassName('projet')[0].classList.remove('visible');
+            document.getElementsByClassName('projet')[0].classList.add('hidden');
+            document.getElementsByClassName('propos')[0].classList.remove('hidden');
+            document.getElementsByClassName('propos')[0].classList.add('visible');
         }
     }
 }
